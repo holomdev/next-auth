3f7d7283d5972b32a1de055bcf17481f94ca2a0a
Fabio Sanchez <holom.dev@gmail.com>
2023-08-14 23:17:55 -0600

    add login and signup page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,51 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
 const LoginPage = () => {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const onLogin = async () => {};
+
   return (
-    <div>LoginPage</div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-center text-2xl">Login</h1>
+      <hr />
+
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-blue-500"
+        type="text"
+        name="email"
+        id="email"
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-blue-500"
+        type="password"
+        name="password"
+        id="password"
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+
+      <button
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-blue-500"
+        onClick={onLogin}>
+        Login here
+      </button>
+      <Link href="/signup">
+        Visit Signup page
+      </Link>
+    </div>
   )
 }
 export default LoginPage
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,61 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
 const SignupPage = () => {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {};
+
   return (
-    <div>SignupPage</div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-center text-2xl">Signup</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-blue-500"
+        type="text"
+        name="username"
+        id="username"
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+      />
+
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-blue-500"
+        type="text"
+        name="email"
+        id="email"
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-blue-500"
+        type="password"
+        name="password"
+        id="password"
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+
+      <button
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-blue-500"
+        onClick={onSignup}>
+        Signup here
+      </button>
+      <Link href="/login">
+        Visit login page
+      </Link>
+    </div>
   )
 }
 export default SignupPage
